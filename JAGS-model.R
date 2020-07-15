09c0dc7c4406b994b55604767a0d9765df4a8aec
Brian Rolek <49486754+BrianRolek@users.noreply.github.com>
2020-07-15 12:02:02 -0300

    Update JAGS-model.R
    remove older comments from old projects

--- JAGS-model.R
+++ JAGS-model.R
@@ -1,5 +1,5 @@
 model{  
-  #### circle effects for CBC ###### 
+  #### Priors ###### 
   mean.r ~ dnorm(0, 0.001)
   mean.lambda <- exp(mean.r)
   tau.proc <-  1/(sig.proc*sig.proc)
@@ -7,7 +7,7 @@ model{
   tau.obs <-  1/(sig.obs*sig.obs)
   sig.obs ~ dunif(0,10)
   
-  #### CBC data model ###### 
+  #### data model ###### 
   for(k in 1 : ncounts) { 	
     lN.est[k] <- lmu.N[yr[k],str[k]]
     logtheta[k] ~ dnorm (lN.est[k], tau.obs)
@@ -18,7 +18,7 @@ model{
   for(s in 1 : nstrata) { 
     #### baseline year and strata expectations ######
     lmn1st[s] <- log(mn1st[s]+0.1) # add a small constant to prevent log(zero) which will result in failure to run
-    lmu.N[1,s] ~ dnorm(lmn1st[s], 0.1) # priors for 1st yr abundance are observed values
+    lmu.N[1,s] ~ dnorm(lmn1st[s], 0.1) # priors for 1st yr abundance are roughly near observed values
     #### dynamics ######
     for( y in startyear[s] : (endyear[s]-1)) { 
       lmu.N[y+1,s] <- lmu.N[y,s]+ r[y,s] 
